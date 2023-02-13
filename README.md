7191745b72566d0e2e11d6a8b6af27f8bf7b9d0a
SutPi0154 <sutpi070@gmail.com>
2023-02-13 16:10:46 +0630

    show-my-name

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # show-my-name
+
+> npm i show-my-name
+
+### Usage
+
+- import myName from "show-my-name"
+- import showMyName from "show-my-name"
+- import showMyNameUI from "show-my-name"
+
---
+++ index.js
@@ -0,0 +1,9 @@
+export const myName = "Nang Sian Sut";
+export const showMyName = () => {
+  return myName;
+};
+export const showMyNameUI = () => {
+  const h1 = document.createElement("h1");
+  h1.innerText = myName;
+  document.body.append(h1);
+};
